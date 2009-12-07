95b39b8675905889cac2b40a36a02857c4508d88
John Edstrom <edstromj@gmail.com>
2009-12-07 00:18:17 +0000

    Forgot to add MBBackupRequest...
    git-svn-id: http://xp-dev.com/svn/dist_fs@39 0c36f658-7202-45cc-8888-a208b6a333a9

---
+++ cs5204/fs/rpc/MBBackupRequest.java
@@ -0,0 +1,44 @@
+package cs5204.fs.rpc;
+
+import cs5204.fs.common.NodeType;
+
+import java.io.Serializable;
+
+/**
+ * Represents a Master-Backup Request to send backup data.
+ */
+public class MBBackupRequest implements Payload
+{
+	private NodeType m_type;
+	private String m_addr;
+    private int m_port;
+	private int m_id;
+	
+	public MBBackupRequest(Nodetype type, String addr, int port, int id)
+	{
+        m_type = type;
+        m_addr = addr;
+        m_port = port;
+		m_id = id;
+	}
+	
+	public NodeType getNodeType()
+	{
+		return m_type;
+	}
+	
+	public String getAddress()
+	{
+		return m_addr;
+	}
+	
+	public int getPort()
+	{
+		return m_port;
+	}
+	
+	public int getId()
+	{
+		return m_id;
+	}
+}
